1b576406320e35a21bc7233fc9d8b78e2af536fb
Louis <louis.malamos@drake.edu>
2015-09-27 19:44:28 -0500

    added Splash Screen and changed screen 6

---
+++ app/src/main/java/com/example/teamawesome/djonutapp/Splash.java
@@ -0,0 +1,44 @@
+package com.example.teamawesome.djonutapp;
+
+/**
+ * Created by Louis on 9/24/2015.
+ */
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by Louis on 9/22/2015.
+ */
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //this.addContentView(R.layout.activity_main);
+
+        setContentView(R.layout.splash_screen);
+        Thread myThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent startMainScreen = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(startMainScreen);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
